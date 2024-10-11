4a9b57b4c90cce84c889bcf058fe0c66ce87bccf
Ezequiel <ezequiel.maranda@gmail.com>
2024-10-11 16:55:10 -0300

    fix(drawer): NavigationDrawer toggle fixed

--- app/src/main/java/com/ifts/navigationdrawer/HomeActivity.kt
+++ app/src/main/java/com/ifts/navigationdrawer/HomeActivity.kt
@@ -1,70 +1,104 @@
 package com.ifts.navigationdrawer
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.MenuItem
+import androidx.activity.OnBackPressedCallback
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import com.ifts.navigationdrawer.databinding.ActivityHomeBinding
 import com.ifts.navigationdrawer.fragments.HomeFragment
 import com.ifts.navigationdrawer.fragments.ProfileFragment
 import com.ifts.navigationdrawer.fragments.SettingsFragment
-import com.ifts.navigationdrawer.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        //setting up the toolbar
-        setSupportActionBar(binding.toolbar)
+        setupToolbar()
+        setupNavigationDrawer()
+        handleBackPress()
 
-        // Config NavigationView & DrawerLayout
-        binding.navView.setNavigationItemSelectedListener { menuItem ->
-            var selectedFragment: Fragment? = null
-            when (menuItem.itemId) {
-                R.id.nav_home -> selectedFragment = HomeFragment()
-                R.id.nav_profile -> selectedFragment = ProfileFragment()
-                R.id.nav_settings -> selectedFragment = SettingsFragment()
-            }
-            if (selectedFragment != null) {
-                supportFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_container, selectedFragment)
-                    .commit()
-            }
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-            true
-        }
-
-        // Initial Fragment
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, HomeFragment())
-                .commit()
-            binding.navView.setCheckedItem(R.id.nav_home)
+            loadInitialFragment()
         }
     }
 
-    @Deprecated("Deprecated in Java")
-    override fun onBackPressed() {
-        if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            binding.drawerLayout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
+    private fun setupToolbar() {
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+    }
+
+    private fun setupNavigationDrawer() {
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            binding.drawerLayout,
+            binding.toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        binding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+
+        binding.navView.setNavigationItemSelectedListener { menuItem ->
+            selectDrawerItem(menuItem)
+            true
         }
+    }
+
+    private fun selectDrawerItem(menuItem: MenuItem) {
+        val fragment: Fragment = when (menuItem.itemId) {
+            R.id.nav_home -> HomeFragment()
+            R.id.nav_profile -> ProfileFragment()
+            R.id.nav_settings -> SettingsFragment()
+            else -> HomeFragment()
+        }
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+
+        menuItem.isChecked = true
+        title = menuItem.title
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+    }
+
+    private fun handleBackPress() {
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+                    binding.drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    isEnabled = false
+                    onBackPressedDispatcher.onBackPressed()
+                }
+            }
+        })
+    }
+
+    private fun loadInitialFragment() {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, HomeFragment())
+            .commit()
+        binding.navView.setCheckedItem(R.id.nav_home)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val TAG = "HomeActivity"
     }
 }
--- app/src/main/java/com/ifts/navigationdrawer/MainActivity.kt
+++ app/src/main/java/com/ifts/navigationdrawer/MainActivity.kt
@@ -14,19 +14,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         binding.btnToHome.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
